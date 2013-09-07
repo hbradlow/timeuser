7a4facb6b6675ccca4af025a44743588a0b36a48
Henry Bradlow <hbradlow@berkeley.edu>
2013-09-06 23:43:47 -0400

    Trying to get on page thing to work

--- harness/activate.js
+++ harness/activate.js
@@ -1,9 +1,11 @@
 function add_to_id(id,s){
     $(id).html(s);
 }
-var background = chrome.extension.getBackgroundPage();
 $(function(){
+    var background = chrome.extension.getBackgroundPage();
     var randomNumber = Math.random();
+    var container = "<div id='container' style='background:black;height:50px;width:50px;position:fixed;top:300px;left:300px;'></div>";
+    $("body").append(container);
     if (randomNumber < 0.5) {
     	background.retrieve_fun_fact("#container");
     }
