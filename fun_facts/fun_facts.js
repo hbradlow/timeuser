1435a37b4abc6d20d4d8e9389c8da69d0b4a13af
Kevin Shin <kevinshin92@gmail.com>
2013-09-06 23:11:18 -0400

    Added fun facts

---
+++ fun_facts/fun_facts.js
@@ -0,0 +1,10 @@
+1) Spawn thread, thread downloads new things, returns as a string or json object to the main
+2) Main thread will take new string eventually and 
+
+function retrieve_fun_fact(id) {
+    var return
+    var prev_data = localStorage["fun_facts"];
+    var facts_link = "http://www.reddit.com/r/todayilearned/top.json?limit=50"
+    var facts_str = 
+    var jQuery.parseJSON( jsonString);
+}
