a4d7465f4ba399d219e0ad977df621f64baacb18
Antoine Balaresque <antoinebalaresque@berkeley.edu>
2013-09-06 23:29:24 -0400

    pacman

---
+++ harness/pacman.js
@@ -0,0 +1,13 @@
+
+function pacman(id){
+    var pacman = " \
+		<canvas id='myCanvas' width='510' height='510'> \
+        </canvas> \
+        <script type='text/javascript' src='../games/Javascript-Pacman/Pacman.js'></script> \
+        <script type='text/javascript' src='../games/Javascript-Pacman/Ghost.js'></script> \
+        <script type='text/javascript' src='../games/Javascript-Pacman/Grid.js'></script> \
+        <script type='text/javascript' src='../games/Javascript-Pacman/Game.js'></script>";
+    
+    var popup = chrome.extension.getViews({type: "popup"})[0];
+    popup.add_to_id(id,pacman);
+}
