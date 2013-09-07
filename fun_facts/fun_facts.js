d031089bc085f96156606c8c6b7a60507ec002fe
Kevin Shin <kevinshin92@gmail.com>
2013-09-06 23:12:52 -0400

    Added fun facts

--- fun_facts/fun_facts.js
+++ fun_facts/fun_facts.js
@@ -1,10 +1,18 @@
-1) Spawn thread, thread downloads new things, returns as a string or json object to the main
-2) Main thread will take new string eventually and 
+var fun_fact_queue = [];
 
 function retrieve_fun_fact(id) {
-    var return
-    var prev_data = localStorage["fun_facts"];
-    var facts_link = "http://www.reddit.com/r/todayilearned/top.json?limit=50"
-    var facts_str = 
-    var jQuery.parseJSON( jsonString);
+    var popup = chrome.extension.getViews({type: "popup"})[0];
+    if (fun_fact_queue.length == 0) {
+        var reddit_str = "";
+        $.get("http://www.reddit.com/r/todayilearned/top.json?limit=50", function(data) {
+            var reddit_arr = data.data.children;
+            var reddit_arr_length = reddit_arr.length;
+            for (var i = 0; i < reddit_arr_length; i++) {
+                fun_fact_queue.push(reddit_arr[i].data.title);
+            }
+            popup.add_to_id(id, fun_fact_queue.pop());
+        });
+    } else {
+        popup.add_to_id(id, fun_fact_queue.pop());
+    }
 }
